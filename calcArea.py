a98f3326f19f5d09c1dbee132c6d4ef113b480b0
vtutya <viniciustutya07@gmail.com>
2025-03-17 21:07:15 -0300

    subindo

--- calcArea.py
+++ calcArea.py
@@ -1,1 +1,29 @@
-# teste conexao de repositorio
+def iniciar():
+    print('Area do triangulo: ')
+    print('Area do retangulo: ')
+    
+    escolha = int(input('Escolha uma opção: '))
+     
+    if escolha == 1:
+        triangulo()
+    elif escolha == 2:
+        retangulo()    
+        
+        
+        
+def triangulo():
+    base = float(input('Digite a base do triangulo: '))
+    altura = float(input('Digite a altura do triangulo: '))   
+    area = (base * altura) / 2   
+    print(area)
+    iniciar() 
+    
+    
+def retangulo():
+    base = float(input('Digite a base do retangulo: '))
+    altura = float(input('Digite a altura do retangulo: '))
+    area = (base * altura)
+    print(area)
+    iniciar()
+
+iniciar()
